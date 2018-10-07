04cf5a5b12010772cd5531539c1dc5f87d08cad3
Shawn Dempsey <shawn@7shifts.com>
2018-10-07 11:31:12 -0600

    rendering example react component

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+        <title>Main</title>
+
+    </head>
+    <body>
+        <div id="example"></div>
+        <script src="js/app.js"></script>
+    </body>
+</html>
